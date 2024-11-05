d9ae92a174f137431b72c18f3c4eccc3a324b7d7
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-11-05 18:03:07 +0100

    First stuff

---
+++ Assets/Model/substances/Technetium99.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace RadiationModel.substances
+{
+    public class Technetium99 : RadioactiveSubstance
+    {
+        public Technetium99() => charge = 0;
+        public Technetium99(int charge) => this.charge = charge;
+        public override string name { get; } = "Technetium-99";
+        public override double halfLife { get; } = 211100 * TimeConstants.SECONDS_IN_YEAR;
+        public override double atomicWeight { get; } = 98.9063;
+        public override Dictionary<double, List<RadioactiveSubstance>> decayProducts { get; } = new();
+    }
+}
