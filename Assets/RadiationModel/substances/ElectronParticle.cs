2cf5846e3cd8b4d8775e5c5343e5c7046c3b7e67
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-11-12 14:00:56 +0100

    Gamma attenuation

--- Assets/RadiationModel/substances/ElectronParticle.cs
+++ Assets/RadiationModel/substances/ElectronParticle.cs
@@ -4,9 +4,12 @@ namespace RadiationModel.substances
 {
     public class ElectronParticle : RadioactiveSubstance
     {
-        public override string name { get; } = "Electron";
-        public override double halfLife { get; } = double.PositiveInfinity;
-        public override double atomicWeight { get; } = 0;
+        public ElectronParticle() => charge = -1;
+        public ElectronParticle(double energy) => this.energy = energy;
+        public override string name => "Electron";
+        public override double halfLife => double.PositiveInfinity;
+        public override double atomicWeight => 0;
+        public double energy { get; }
         public override Dictionary<double, List<RadioactiveSubstance>> decayProducts { get; } = new();
     }
 }
