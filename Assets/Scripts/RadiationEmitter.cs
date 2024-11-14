36fe7bd0442d0f9e0eb9625a84711713ee768b53
Gulp1n <abeldenouden@hotmail.com>
2024-11-14 12:50:05 +0100

    init scene

--- Assets/Scripts/RadiationEmitter.cs
+++ Assets/Scripts/RadiationEmitter.cs
@@ -18,6 +18,10 @@ public class RadiationEmitter : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        if (emitting)
+        {
+            Emit();
+        }
     }
 
     // Update is called once per frame
