d6e82ad2c37d6d1c4f68c1e12711c50c14a3be8b
Gulp1n <abeldenouden@hotmail.com>
2024-11-19 13:27:20 +0100

    wow, afstandBeta, so cool!

--- Assets/Scripts/RadiationEmitter.cs
+++ Assets/Scripts/RadiationEmitter.cs
@@ -20,6 +20,7 @@ public class RadiationEmitter : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        if (emitting) Emit();
     }
 
     // Update is called once per frame
